cb919c432b527b8b8b114e977965edb78d583c23
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2018-11-25 02:26:41 +0100

    Add output

--- main.clj
+++ main.clj
@@ -9,9 +9,15 @@
         r (->> lines last parse-line to-array)]
     {:n n :a a :r r}))
 
+(defn stringify-array [a] (-> a seq prn-str (str/replace #"[\(\)]" "")))
+
 (defn output [data]
-  (->> (data :a) seq (map seq) println)
-  (-> (data :r) seq (into []) println))
+  (spit
+    "output.txt"
+    (str
+      (prn-str (data :n))
+      (->> (data :a) seq (map stringify-array) (str/join))
+      (stringify-array (data :r)))))
 
 (defn scalar [i j a s] (aset s i (/ (aget a i j) (aget a j j))))
 
@@ -36,7 +42,13 @@
   (let [data (read-input "input.txt")
         scalars (->> (data :n) (range 0) to-array)]
     (dotimes [j (data :n)]
-      (go (data :n) (fn [i] (fn [] (scalar i j (data :a) scalars))) (fn [i] (not= i j)))
-      (go (data :n) (fn [i] (fn [] (zeroes j i (data :a) (aget scalars i) (data :r)))) (fn [i] (not= i j))))
+      (go
+        (data :n)
+        (fn [i] (fn [] (scalar i j (data :a) scalars)))
+        (fn [i] (not= i j)))
+      (go
+        (data :n)
+        (fn [i] (fn [] (zeroes j i (data :a) (aget scalars i) (data :r))))
+        (fn [i] (not= i j))))
     (go (data :n) (fn [i] (fn [] (division i (data :a) (data :r)))) (fn [i] true))
     (output data)))
