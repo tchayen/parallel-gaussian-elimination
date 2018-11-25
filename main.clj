8379d0379130984dcc92855e95be00762a14b885
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2018-11-25 01:47:47 +0100

    Fix bugs; refactor code

--- main.clj
+++ main.clj
@@ -9,50 +9,34 @@
         r (->> lines last parse-line to-array)]
     {:n n :a a :r r}))
 
+(defn output [data]
+  (->> (data :a) seq (map seq) println)
+  (-> (data :r) seq (into []) println))
+
 (defn scalar [i j a s] (aset s i (/ (aget a i j) (aget a j j))))
 
-(defn zeroes [i j a s]
+(defn zeroes [i j a s r]
   (dotimes [k (alength a)]
-    (aset a j k (- (aget a j k) (* (get s i) (aget a i k))))))
+    (aset a j k (- (aget a j k) (* s (aget a i k)))))
+  (aset r j (- (aget r j) (* s (aget r i)))))
 
-(defn division [i a r]
-  (aset r i (/ (aget r i) (aget a i i)))
-  (aset a i i 1))
+(defn division [i a r] (aset r i (/ (aget r i) (aget a i i))) (aset a i i 1))
+
+(defn go [n fu fi]
+  (let [threads
+        (->>
+          (range 0 n)
+          (filter fi)
+          (map fu)
+          (map #(Thread. %)))]
+    (run! #(.start %) threads)
+    (run! #(.join %) threads)))
 
 (defn -main [& args]
   (let [data (read-input "input.txt")
         scalars (->> (data :n) (range 0) to-array)]
     (dotimes [j (data :n)]
-      (let [threads
-            (->>
-              (range 0 (data :n))
-              (filter #(not= % j))
-              (map #(fn [] (scalar % j (data :a) scalars)))
-              (map #(Thread. %)))]
-        ; (doseq [i (seq threads)] (i))
-        (run! #(.start %) threads)
-        (run! #(.join %) threads))
-
-      (let [threads
-            (->>
-              (range 0 (data :n))
-              (filter #(not= % j))
-              (map #(fn [] (zeroes % j (data :a) scalars)))
-              (map #(Thread. %)))]
-        (run! #(.start %) threads)
-        (run! #(.join %) threads))
-
-      (let [threads
-            (->>
-              (range 0 (data :n))
-              (filter #(not= % j))
-              (map #(fn [] (division % (data :a) (data :r))))
-              (map #(Thread. %)))]
-        (run! #(.start %) threads)
-        (run! #(.join %) threads))
-    )
-
-        (-> scalars seq println)
-        (->> (data :a) seq (map seq) println)
-        (-> (data :r) seq println)
-        ))
+      (go (data :n) (fn [i] (fn [] (scalar i j (data :a) scalars))) (fn [i] (not= i j)))
+      (go (data :n) (fn [i] (fn [] (zeroes j i (data :a) (aget scalars i) (data :r)))) (fn [i] (not= i j))))
+    (go (data :n) (fn [i] (fn [] (division i (data :a) (data :r)))) (fn [i] true))
+    (output data)))
